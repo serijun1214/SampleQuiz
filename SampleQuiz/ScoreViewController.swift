f85e54e0ee66197b371d666551fa174b1fa1c5e1
serijun1214 <serijun.1214+github@gmail.com>
2024-09-12 12:50:40 +0900

    スコア画面で正解数を表示

--- SampleQuiz/ScoreViewController.swift
+++ SampleQuiz/ScoreViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class ScoreViewController: UIViewController {
-
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var correct = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scoreLabel.text = "\(correct)問正解!"
 
         // Do any additional setup after loading the view.
     }
