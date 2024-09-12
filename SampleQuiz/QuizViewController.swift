2b1885ef3ce26eeb501157c1e8d2cd80f3bfd164
serijun1214 <serijun.1214+github@gmail.com>
2024-09-12 13:04:23 +0900

    正誤判定時の画像表示

--- SampleQuiz/QuizViewController.swift
+++ SampleQuiz/QuizViewController.swift
@@ -14,6 +14,7 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var answerButton2: UIButton!
     @IBOutlet weak var answerButton3: UIButton!
     @IBOutlet weak var answerButton4: UIButton!
+    @IBOutlet weak var judgeImageView: UIImageView!
     
     var csvArray: [String] = []
     var quizArray: [String] = []
@@ -48,8 +49,14 @@ class QuizViewController: UIViewController {
         if sender.tag == Int(quizArray[1]) {
             print("正解")
             correctCount += 1
+            judgeImageView.image = UIImage(named: "correct")
         } else {
             print("不正解")
+            judgeImageView.image = UIImage(named: "incorrect")
+        }
+        judgeImageView.isHidden = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.judgeImageView.isHidden = true
         }
         nextQuiz()
     }
